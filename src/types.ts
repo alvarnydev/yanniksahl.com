786912fd720448343c705495c79b80200044eeb0
Yannik Sahl <me@yanniksahl.com>
2024-01-11 12:50:33 +0100

    Education DegreeCard

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export interface Degree {
+  title: string;
+  image: string;
+  when: string;
+  where: string;
+  special: string;
+  grade: string;
+}
+
+export interface Experience {
+  role: string;
+  when: string;
+  where: string;
+  company: string;
+  tasks: string;
+}
