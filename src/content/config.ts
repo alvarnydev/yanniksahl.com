339ed20b42d6b0216378a9c68781979d24b8f80d
Yannik Sahl <me@yanniksahl.com>
2024-01-11 12:02:54 +0100

    WorkNav feature

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,31 @@
 import { defineCollection, z } from "astro:content";
 
+const devCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.coerce.date(),
+    tags: z.array(z.string()),
+    link: z.string().optional(),
+    img: z.string(),
+    img_alt: z.string().optional(),
+  }),
+});
+const musicCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.coerce.date(),
+    tags: z.array(z.string()),
+    link: z.string().optional(),
+    img: z.string(),
+    img_alt: z.string().optional(),
+  }),
+});
+
 export const collections = {
-  about: defineCollection({
-    type: "content",
-    schema: z.object({
-      title: z.string(),
-    }),
-  }),
-  work: defineCollection({
-    type: "content",
-    schema: z.object({
-      title: z.string(),
-      description: z.string(),
-      publishDate: z.coerce.date(),
-      tags: z.array(z.string()),
-      link: z.string().optional(),
-      img: z.string(),
-      img_alt: z.string().optional(),
-    }),
-  }),
+  dev: devCollection,
+  music: musicCollection,
 };
